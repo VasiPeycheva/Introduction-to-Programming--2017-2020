df610cfe2e8b6ae8a797db470db94a9842ad35bb
VasiPeycheva <vasilena.peicheva@gmail.com>
2017-11-03 15:39:57 +0200

    Practice_5

---
+++ Practice_5/task12_PrimeDevisors.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+int main() {
+
+	unsigned int N;
+
+	std::cin >> N;
+
+	std::cout << "N = ";
+
+	for (int i = 2; i <= N; i++) {
+
+		while (N % i == 0) {
+
+			std::cout << i;
+			N /= i;
+
+			if (N != 1) //still have divisors 
+				std::cout << " * ";
+			
+
+		}
+
+	}
+
+	std::cout << '\n';
+
+
+	return 0;
+}
