95eeb1e7123c045aef7c07fd56923ef6bab1785c
pminev <p.minev.96@abv.bg>
2017-11-24 00:40:32 +0200

    Fix HighestProd solution

--- HomeWork - Defense 1/HighestProd - Task2 GroupB.cpp
+++ HomeWork - Defense 1/HighestProd - Task2 GroupB.cpp
@@ -2,18 +2,18 @@
 /*
 	Second Task :B
 	Write a program which takes from input positive integer N. Then the program should take
-	N integers. You should print the highest product of three consecutive elements.
-	Examples:			Input:					Output:
-						5  2 5 4 3 5				60
-						7  5 6 0 10 1 15 -3			150
-						5  1 2 8 4 5				160
+	N integers. You should print the highest product of three consecutive elements smaller than 10^3.
+	Examples:           Input:                      Output:
+	                    5  2 5 4 3 5                  60
+	                    7  5 6 0 10 1 15 -3           150
+	                    5  1 2 8 4 5                  160
 */
 
 int main()
 {
 	unsigned int number;
 	short prePrev=1, prev=1, curr=1;
-	int maxProd = 0, curProd = 0;
+	int maxProd = -1000*1000*1000 - 10, curProd = -1000*1000*1000 - 10;
 
 	std::cin >> number;
 
