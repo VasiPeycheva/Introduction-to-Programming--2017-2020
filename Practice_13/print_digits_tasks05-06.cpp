73a385481bc565c8b10a0e5d7001a449f8f53c01
VasiPeycheva <vasilena.peicheva@gmail.com>
2018-01-12 22:36:15 +0200

    add solution to task3/4/5/6

---
+++ Practice_13/print_digits_tasks05-06.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+// PAY ATTENTION how swapping two code lines changes the whole program
+
+/*************** Task 5 ***************/
+void printForward(unsigned int number)
+{
+	if (number <= 0)
+		return;
+	printForward(number / 10);
+	cout << (number % 10) << " ";
+}
+
+/*************** Task 6 ***************/
+void printBackward(unsigned int number)
+{
+	if (number <= 0)
+		return;
+	cout << (number % 10) << " ";
+	printBackward(number / 10);
+}
+
+
+int main()
+{
+	unsigned int number;
+	cin >> number;
+	
+	// task 5
+	cout << "Forward: "; 
+	printForward(number);
+	cout << endl;
+
+	// task 6
+	cout << "Backward: ";
+	printBackward(number);
+	cout << endl;
+}
