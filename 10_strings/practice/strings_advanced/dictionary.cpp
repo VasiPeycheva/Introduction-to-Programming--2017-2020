f7ef5026d316087cc4d70b8a85f38e21ec23510d
Kristian-Krastev <44848168+Kristian-Krastev@users.noreply.github.com>
2020-01-13 23:07:02 +0200

    adding solutions for advanced strings

---
+++ 10_strings/practice/strings_advanced/dictionary.cpp
@@ -0,0 +1,41 @@
+#include <iostream> // cin, cout
+
+#include <string.h> // strlen, strcat 
+
+const unsigned int WORD_CNT = 3;
+const unsigned int MAX_INPUT = 1024;
+
+bool check_dic(const char* dic[], const char* word) {
+	for (size_t i = 0; i < WORD_CNT; i++)
+		if (strcmp(word, dic[i]) == 0) 	// if the word on position i 
+			return true;				//in the dicionary is as same as the given one
+
+	return false;
+}
+
+int main() {
+
+	const char* dic[] = { "cats", "dogs", "animals" };
+
+	char input[MAX_INPUT];	// "it is raining cats and dogs";
+
+	std::cin.getline(input, MAX_INPUT); //reading the whole input
+
+	//for each word form it, check into the dictionary
+
+	//takes the first word
+	char* curr_word = strtok(input, " ");
+	unsigned int how_many = 0;
+	while (curr_word != nullptr) {
+
+		if (check_dic(dic, curr_word))
+			how_many++;
+
+		// takes the next word form input string
+		curr_word = strtok(nullptr, " "); 
+	}
+
+	std::cout << how_many << '\n';
+
+	return 0;
+}
---
+++ 10_strings/practice/strings_advanced/sequence.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string.h>
+
+size_t get_same_symb_cnt(const char* str) {
+
+	size_t cnt = 1;
+	while (*str != '\0' && *str == *(str + 1)) {
+		++str;
+		++cnt;
+	}
+
+	return cnt;
+}
+
+void resize(char*& str, const char* next_symbols) {
+	
+	if (str == nullptr) {
+		str = new char[strlen(next_symbols) + 1];
+		strcpy(str, next_symbols);
+		return;
+	}
+
+	size_t len = strlen(str);
+	size_t more_symb = strlen(next_symbols);
+	char* new_str = new char[len + more_symb + 1];
+	strcpy(new_str, str);
+	strcat(new_str, next_symbols);
+
+	delete[] str;
+	str = new_str;
+}
+
+char* get_sequence(size_t n) {
+
+	char* result_str = new char[2];
+	strcpy(result_str, "1");
+
+	const char* temp_ptr = result_str;
+	
+	char* temp_str = nullptr;
+
+	char temp[32];
+
+	for (size_t i = 1; i < n; i++) {
+
+		while (*temp_ptr != '\0') {
+
+			size_t symb_cnt = get_same_symb_cnt(temp_ptr);
+
+			temp[0] = '0' + symb_cnt;
+			temp[1] = *temp_ptr;
+			temp[2] = '\0';
+
+			resize(temp_str, temp);
+			temp_ptr += symb_cnt;
+		}
+		delete[] result_str;
+		result_str = new char[strlen(temp_str) + 1];
+		strcpy(result_str, temp_str);
+		delete[] temp_str;
+		temp_str = nullptr;
+		temp_ptr = result_str;
+	}
+
+	delete[] temp_str;
+	return result_str;
+}
+
+int main() {
+
+	char* sequence = get_sequence(5);
+	
+	return 0;
+}
